727d2b080ee90e3c8cd9433f33af142827146295
ThummanapallyGouthami <tgouthami2004@gmail.com>
2026-09-14 18:37:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Registration from "./Registration";
+import Dashboard from "./Dashboard";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Registration.jsx
@@ -0,0 +1,20 @@
+function Registration() {
+  return (
+    <div>
+      <h1>Registration Page</h1>
+
+      <input type="text" placeholder="Name" />
+      <br /><br />
+
+      <input type="email" placeholder="Email" />
+      <br /><br />
+
+      <input type="password" placeholder="Password" />
+      <br /><br />
+
+      <button>Register</button>
+    </div>
+  );
+}
+
+export default Registration;
